1b1d70bbbb2ff1d7a1f32a502c69dbe0e6d4f38c
KELVIN MURITHI <kelvinmurithi@live.com>
2024-01-14 18:48:32 +0300

    Add Hero, Power, and HeroPower models with relationships

--- app/models.py
+++ app/models.py
@@ -1,10 +1,27 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
 class Hero(db.Model):
-    __tablename__ = 'hero'
-
+    __tablename__ = 'heroes'
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    super_name = db.Column(db.String(50))
+    powers = relationship('HeroPower', back_populates='hero')
 
-# add any models you may need. 
+class Power(db.Model):
+    __tablename__ = 'powers'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    description = db.Column(db.String(120), nullable=False)
+    heroes = relationship('HeroPower', back_populates='power')
+
+class HeroPower(db.Model):
+    __tablename__ = 'hero_powers'
+    id = db.Column(db.Integer, primary_key=True)
+    strength = db.Column(db.Enum('Strong', 'Weak', 'Average'), nullable=False)
+    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'))
+    power_id = db.Column(db.Integer, db.ForeignKey('powers.id'))
+    hero = relationship('Hero', back_populates='powers')
+    power = relationship('Power', back_populates='heroes')
