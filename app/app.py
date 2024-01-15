4c33a0c8da70e095957add771c40a3a86120e1dd
KELVIN MURITHI <kelvinmurithi@live.com>
2024-01-15 16:02:07 +0300

    Refactor to use Marshmallow for serialization

--- app/app.py
+++ app/app.py
@@ -1,14 +1,17 @@
 from flask import Flask, make_response, jsonify, request
 from flask_migrate import Migrate
-from models import db, Hero, Power, HeroPower
+from models import db, Hero, Power, HeroPower, HeroSchema, PowerSchema, HeroPowerSchema
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 migrate = Migrate(app, db)
-
 db.init_app(app)
+
+# Initializes marshmallow
+from models import ma
+ma.init_app(app)
 
 @app.route('/')
 def home():
@@ -18,38 +21,22 @@ def home():
 @app.route('/heroes', methods=['GET'])
 def get_heroes():
     heroes = Hero.query.all()
-    hero_dict = [
-        {"id": hero.id, "name": hero.name, "super_name": hero.super_name}
-        for hero in heroes
-    ]
-    return make_response(jsonify(hero_dict), 200)
+    hero_schema = HeroSchema(many=True)
+    return make_response(jsonify(hero_schema.dump(heroes)), 200)
 
 @app.route('/heroes/<int:id>', methods=['GET'])
 def get_hero(id):
     hero = Hero.query.get(id)
     if hero is None:
         return make_response(jsonify({"error": "Hero not found"}), 404)
-    powers_dict = [
-        {"id": hero_power.power.id, "name": hero_power.power.name, "description": hero_power.power.description}
-        for hero_power in hero.powers
-    ]
-    hero_dict = {
-        "id": hero.id,
-        "name": hero.name,
-        "super_name": hero.super_name,
-        "powers": powers_dict
-    }
-    return make_response(jsonify(hero_dict), 200)
-
+    hero_schema = HeroSchema()
+    return make_response(jsonify(hero_schema.dump(hero)), 200)
 
 @app.route('/powers', methods=['GET'])
 def get_powers():
     powers = Power.query.all()
-    powers_dict = [
-        {"id": power.id, "name": power.name, "description": power.description}
-        for power in powers
-    ]
-    return make_response(jsonify(powers_dict), 200)
+    power_schema = PowerSchema(many=True)
+    return make_response(jsonify(power_schema.dump(powers)), 200)
 
 @app.route('/powers/<int:id>', methods=['GET', 'PATCH'])
 def get_or_update_power(id):
@@ -63,12 +50,8 @@ def get_or_update_power(id):
             db.session.commit()
         else:
             return make_response(jsonify({"errors": ["validation errors"]}), 400)
-    power_dict = {
-        "id": power.id,
-        "name": power.name,
-        "description": power.description
-    }
-    return make_response(jsonify(power_dict), 200)
+    power_schema = PowerSchema()
+    return make_response(jsonify(power_schema.dump(power)), 200)
 
 @app.route('/hero_powers', methods=['POST'])
 def create_hero_power():
@@ -80,17 +63,8 @@ def create_hero_power():
         db.session.add(hero_power)
         db.session.commit()
         hero = Hero.query.get(hero_id)
-        powers_dict = [
-            {"id": power.id, "name": power.name, "description": power.description}
-            for power in hero.powers
-        ]
-        hero_dict = {
-            "id": hero.id,
-            "name": hero.name,
-            "super_name": hero.super_name,
-            "powers": powers_dict
-        }
-        return make_response(jsonify(hero_dict), 200)
+        hero_schema = HeroSchema()
+        return make_response(jsonify(hero_schema.dump(hero)), 200)
     else:
         return make_response(jsonify({"errors": ["validation errors"]}), 400)
 
