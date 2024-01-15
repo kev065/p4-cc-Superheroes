45b2568bc301e47e070b3c4f1a829a2d42c605e4
KELVIN MURITHI <kelvinmurithi@live.com>
2024-01-15 10:40:15 +0300

    Add validation checks to HeroPower model

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, validates
 from sqlalchemy import event
 from sqlalchemy.sql import func
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -32,10 +33,17 @@ def validate_description(target, value, oldvalue, initiator):
 class HeroPower(db.Model):
     __tablename__ = 'hero_powers'
     id = db.Column(db.Integer, primary_key=True)
-    strength = db.Column(db.Enum('Strong', 'Weak', 'Average'), nullable=False)
-    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'))
-    power_id = db.Column(db.Integer, db.ForeignKey('powers.id'))
-    hero = relationship('Hero', back_populates='powers')
-    power = relationship('Power', back_populates='heroes')
-    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
-    updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())
+    strength = db.Column(db.String(255), nullable=False)
+    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'), nullable=False)
+    power_id = db.Column(db.Integer, db.ForeignKey('powers.id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, server_default=db.func.now(), onupdate=datetime.utcnow)
+    hero = db.relationship('Hero', back_populates='powers')
+    power = db.relationship('Power', back_populates='heroes')
+
+    @validates('strength')
+    def validate_strength(self, key, strength):
+        strength_list = ['Strong', 'Weak', 'Average']
+        if not any(substring in strength for substring in strength_list):
+            raise ValueError("Invalid strength value. Accepted values are 'Strong', 'Weak', or 'Average'.")
+        return strength
